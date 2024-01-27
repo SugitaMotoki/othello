d3dec4114444e36695e7977f230a88481b20694e
SugitaMotoki <ce235019@meiji.ac.jp>
2024-01-27 23:32:59 +0900

    選択肢の1番目を選ぶ状態

--- agents/a01.c
+++ agents/a01.c
@@ -17,34 +17,97 @@ typedef enum board_state {
     WALL                        /* 3: 壁 */
 } BOARD_STATE;
 
+#define ANOTHER_STONE(S) ((BLACK) + (WHITE) - (S))
+
 typedef struct board {
     BOARD_STATE array[BOARD_WIDTH * BOARD_HEIGHT];
 } BOARD;
 
 typedef struct node {
-    BOARD_STATE stone;
     BOARD board;
+    BOARD_STATE stone;
+    char next_choices[FIELD_SIZE * FIELD_SIZE];	/* 数字を格納するが、メモリ節約のためchar型 */
 } NODE;
 
 /* boardで隣へ移動する際に添え字に足す数字（8方位） */
 static int directions[8] = { 10, 11, 1, -9, -10, -11, -1, 9 };
 
-static void init_root_node(NODE * root, const char *first_board_str)
+static int count_next_choices(char *next_choices)
 {
-    int x, y;
     int i = 0;
-    for (x = 0; x < BOARD_HEIGHT; x++) {
-        for (y = 0; y < BOARD_WIDTH; y++) {
-            if (x == 0 || x == BOARD_HEIGHT - 1 || y == 0
-                || y == BOARD_WIDTH - 1) {
-                root->board.array[BOARD_I(x, y)] = WALL;
-            } else {
-                root->board.array[BOARD_I(x, y)] =
-                    first_board_str[i] - '0';
-                i++;
+    while (next_choices[i] != -1) {
+        i++;
+    }
+    return i;
+}
+
+static void push_next_choices(const int board_i, char *next_choices)
+{
+    int j = count_next_choices(next_choices);
+    next_choices[j] = board_i;
+    next_choices[j + 1] = -1;
+    return;
+}
+
+static int count_reversibles_one_dir(const int x, const int y,
+                                     const BOARD_STATE stone,
+                                     const BOARD board, const int dir_i)
+{
+    BOARD_STATE current;
+    int count = 0;
+    int i = BOARD_I(x, y);
+    while (1) {
+        i += directions[dir_i];
+        current = board.array[i];
+        if (current == EMPTY || current == WALL) {
+            return 0;
+        } else if (current == stone) {
+            return count;
+        } else {
+            count++;
+        }
+    }
+}
+
+static bool can_put_stone(const int x, const int y,
+                          const BOARD_STATE stone, const BOARD board)
+{
+    int d;
+    if (board.array[BOARD_I(x, y)] != EMPTY) {
+        return false;
+    }
+
+    for (d = 0; d < 8; d++) {
+        if (count_reversibles_one_dir(x, y, stone, board, d)) {
+            return true;
+        }
+    }
+    return false;
+}
+
+static void search_next_choices(NODE * node)
+{
+    int x, y, d;
+    for (x = 1; x < BOARD_HEIGHT - 1; x++) {
+        for (y = 1; y < BOARD_WIDTH - 1; y++) {
+            if (can_put_stone(x, y, node->stone, node->board)) {
+                push_next_choices(BOARD_I(x, y), node->next_choices);
             }
         }
     }
+}
+
+static void print_next_choices(const char *next_choices)
+{
+    int j = 0;
+    int i = next_choices[j];
+
+    while (i != -1) {
+        printf("(%d,%d) ", BOARD_X(i) - 1, BOARD_Y(i) - 1);
+        j++;
+        i = next_choices[j];
+    }
+    printf("\n");
 }
 
 static void print_board_state_icon(const BOARD_STATE state)
@@ -68,7 +131,7 @@ static void print_board_state_icon(const BOARD_STATE state)
     }
 }
 
-static void print_board(BOARD board)
+static void print_board(const BOARD board)
 {
     int x, y;
 
@@ -88,16 +151,40 @@ static void print_board(BOARD board)
     }
 }
 
+static void init_root_node(NODE * root, const char *argv_1)
+{
+    int x, y;
+    int i = 0;
+    for (x = 0; x < BOARD_HEIGHT; x++) {
+        for (y = 0; y < BOARD_WIDTH; y++) {
+            if (x == 0 || x == BOARD_HEIGHT - 1 || y == 0
+                || y == BOARD_WIDTH - 1) {
+                root->board.array[BOARD_I(x, y)] = WALL;
+            } else {
+                root->board.array[BOARD_I(x, y)] = argv_1[i] - '0';
+                i++;
+            }
+        }
+    }
+
+    root->stone = argv_1[65] - '0';
+    root->next_choices[0] = -1;
+}
+
 int main(int argc, char *argv[])
 {
     NODE root;
-    BOARD_STATE stone;
-
-    stone = argv[1][65] - '0';
+    char result_x, result_y;
 
     init_root_node(&root, argv[1]);
-
+    search_next_choices(&root);
     print_board(root.board);
+    print_next_choices(root.next_choices);
+
+    result_x = BOARD_X(root.next_choices[0]) - 1;
+    result_y = BOARD_Y(root.next_choices[0]) - 1;
+
+    printf("{x:%d,y:%d}\n", result_x, result_y);
 
     return 0;
 }
