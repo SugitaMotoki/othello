180c9502dbfc9d88f546de72df0a40b07420e9b5
SugitaMotoki <ce235019@meiji.ac.jp>
2024-01-28 01:07:52 +0900

    Board_I型を適用

--- agents/a01.c
+++ agents/a01.c
@@ -6,7 +6,7 @@
 #define FIELD_SIZE (8)          /* オセロのルール上の盤の大きさ */
 #define BOARD_WIDTH ((FIELD_SIZE) + 2)	/* 両端の壁を含めた幅 */
 #define BOARD_HEIGHT ((FIELD_SIZE) + 2)	/* 両端の壁を含めた高さ */
-#define BOARD_I(X,Y) ((X) * (BOARD_HEIGHT) + (Y))
+#define GET_BOARD_I(X,Y) ((X) * (BOARD_HEIGHT) + (Y))
 #define BOARD_X(I) ((I) / (BOARD_HEIGHT))
 #define BOARD_Y(I) ((I) % (BOARD_HEIGHT))
 
@@ -23,17 +23,19 @@ typedef struct board {
     BOARD_STATE array[BOARD_WIDTH * BOARD_HEIGHT];
 } BOARD;
 
+typedef char Board_I;
+
 typedef struct node {
-    BOARD board;
     BOARD_STATE stone;
-    char next_choices[FIELD_SIZE * FIELD_SIZE];	/* 数字を格納するが、メモリ節約のためchar型 */
+    BOARD board;
+    Board_I next_choices[FIELD_SIZE * FIELD_SIZE];	/* 数字を格納するが、メモリ節約のためchar型 */
     struct node **children;
 } NODE;
 
 /* boardで隣へ移動する際に添え字に足す数字（8方位） */
-static int directions[8] = { 10, 11, 1, -9, -10, -11, -1, 9 };
+static Board_I directions[8] = { 10, 11, 1, -9, -10, -11, -1, 9 };
 
-static int count_next_choices(char *next_choices)
+static int count_next_choices(Board_I * next_choices)
 {
     int i = 0;
     while (next_choices[i] != -1) {
@@ -42,7 +44,8 @@ static int count_next_choices(char *next_choices)
     return i;
 }
 
-static void push_next_choices(const int board_i, char *next_choices)
+static void push_next_choices(const Board_I board_i,
+                              Board_I * next_choices)
 {
     int j = count_next_choices(next_choices);
     next_choices[j] = board_i;
@@ -50,17 +53,18 @@ static void push_next_choices(const int board_i, char *next_choices)
     return;
 }
 
-static int count_reversibles_in_the_direction(const char board_i,
+static int count_reversibles_in_the_direction(const Board_I i,
                                               const BOARD_STATE stone,
                                               const BOARD * board,
                                               const int dir_i)
 {
     BOARD_STATE current;
+    Board_I j = i;
     int count = 0;
-    int i = board_i;
+
     while (1) {
-        i += directions[dir_i];
-        current = board->array[i];
+        j += directions[dir_i];
+        current = board->array[j];
         if (current == EMPTY || current == WALL) {
             return 0;
         } else if (current == stone) {
@@ -71,16 +75,17 @@ static int count_reversibles_in_the_direction(const char board_i,
     }
 }
 
-static bool can_put_stone(const char board_i,
+static bool can_put_stone(const Board_I board_i,
                           const BOARD_STATE stone, const BOARD * board)
 {
-    int d;
+    int dir_i;
     if (board->array[board_i] != EMPTY) {
         return false;
     }
 
-    for (d = 0; d < 8; d++) {
-        if (count_reversibles_in_the_direction(board_i, stone, board, d)) {
+    for (dir_i = 0; dir_i < 8; dir_i++) {
+        if (count_reversibles_in_the_direction
+            (board_i, stone, board, dir_i)) {
             return true;
         }
     }
@@ -89,53 +94,52 @@ static bool can_put_stone(const char board_i,
 
 static void search_next_choices(NODE * node)
 {
-    int x, y, d;
+    int x, y;
     for (x = 1; x < BOARD_HEIGHT - 1; x++) {
         for (y = 1; y < BOARD_WIDTH - 1; y++) {
-            if (can_put_stone(BOARD_I(x, y), node->stone, &node->board)) {
-                push_next_choices(BOARD_I(x, y), node->next_choices);
+            if (can_put_stone
+                (GET_BOARD_I(x, y), node->stone, &node->board)) {
+                push_next_choices(GET_BOARD_I(x, y), node->next_choices);
             }
         }
     }
 }
 
-static void reverse_stone_in_the_direction(const char board_i,
+static void reverse_stone_in_the_direction(const Board_I i,
                                            BOARD_STATE stone,
-                                           BOARD * board,
-                                           const int direction_i)
+                                           BOARD * board, const int dir_i)
 {
-    int i = board_i;
-    i += directions[direction_i];
-    while (board->array[i] != stone) {
-        board->array[i] = stone;
-        i += directions[direction_i];
+    Board_I j = i;
+    j += directions[dir_i];
+    while (board->array[j] != stone) {
+        board->array[j] = stone;
+        j += directions[dir_i];
     }
 }
 
-static void reverse_stone(const char board_i, BOARD_STATE stone,
+static void reverse_stone(const Board_I board_i, BOARD_STATE stone,
                           BOARD * board)
 {
-    int direction_i;
-    for (direction_i = 0; direction_i < 8; direction_i++) {
+    int dir_i;
+    for (dir_i = 0; dir_i < 8; dir_i++) {
         if (count_reversibles_in_the_direction
-            (board_i, stone, board, direction_i)
+            (board_i, stone, board, dir_i)
             != 0) {
-            reverse_stone_in_the_direction(board_i, stone, board,
-                                           direction_i);
+            reverse_stone_in_the_direction(board_i, stone, board, dir_i);
         }
     }
 }
 
-static void put_stone(const char board_i, BOARD_STATE stone, BOARD * board)
+static void put_stone(const Board_I i, BOARD_STATE stone, BOARD * board)
 {
-    board->array[board_i] = stone;
-    reverse_stone(board_i, stone, board);
+    board->array[i] = stone;
+    reverse_stone(i, stone, board);
 }
 
-static void print_next_choices(const char *next_choices)
+static void print_next_choices(const Board_I * next_choices)
 {
     int j = 0;
-    int i = next_choices[j];
+    Board_I i = next_choices[j];
 
     while (i != -1) {
         printf("(%d,%d) ", BOARD_X(i) - 1, BOARD_Y(i) - 1);
@@ -179,7 +183,7 @@ static void print_board(const BOARD board)
     for (x = 1; x < BOARD_HEIGHT - 1; x++) {
         printf(" %d|", x - 1);
         for (y = 1; y < BOARD_WIDTH - 1; y++) {
-            print_board_state_icon(board.array[BOARD_I(x, y)]);
+            print_board_state_icon(board.array[GET_BOARD_I(x, y)]);
             printf("|");
         }
         printf("\n");
@@ -188,8 +192,8 @@ static void print_board(const BOARD board)
 
 static void create_child_node(NODE * node)
 {
-    char number_of_next_choices = count_next_choices(node->next_choices);
-    char i;
+    int number_of_next_choices = count_next_choices(node->next_choices);
+    Board_I i;
 
     node->children = malloc(sizeof(NODE *) * number_of_next_choices);
 
@@ -206,14 +210,14 @@ static void create_child_node(NODE * node)
 static void init_root_node(NODE * root, const char *argv_1)
 {
     int x, y;
-    int i = 0;
+    Board_I i = 0;
     for (x = 0; x < BOARD_HEIGHT; x++) {
         for (y = 0; y < BOARD_WIDTH; y++) {
             if (x == 0 || x == BOARD_HEIGHT - 1 || y == 0
                 || y == BOARD_WIDTH - 1) {
-                root->board.array[BOARD_I(x, y)] = WALL;
+                root->board.array[GET_BOARD_I(x, y)] = WALL;
             } else {
-                root->board.array[BOARD_I(x, y)] = argv_1[i] - '0';
+                root->board.array[GET_BOARD_I(x, y)] = argv_1[i] - '0';
                 i++;
             }
         }
