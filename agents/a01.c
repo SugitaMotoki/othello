a9370a136f57e7fcbb0f8ab72a2b033826f9fdc9
SugitaMotoki <ce235019@meiji.ac.jp>
2024-01-28 00:44:57 +0900

    次の手の予測機能を追加

--- agents/a01.c
+++ agents/a01.c
@@ -27,6 +27,7 @@ typedef struct node {
     BOARD board;
     BOARD_STATE stone;
     char next_choices[FIELD_SIZE * FIELD_SIZE];	/* 数字を格納するが、メモリ節約のためchar型 */
+    struct node **children;
 } NODE;
 
 /* boardで隣へ移動する際に添え字に足す数字（8方位） */
@@ -49,16 +50,17 @@ static void push_next_choices(const int board_i, char *next_choices)
     return;
 }
 
-static int count_reversibles_one_dir(const int x, const int y,
-                                     const BOARD_STATE stone,
-                                     const BOARD board, const int dir_i)
+static int count_reversibles_in_the_direction(const char board_i,
+                                              const BOARD_STATE stone,
+                                              const BOARD * board,
+                                              const int dir_i)
 {
     BOARD_STATE current;
     int count = 0;
-    int i = BOARD_I(x, y);
+    int i = board_i;
     while (1) {
         i += directions[dir_i];
-        current = board.array[i];
+        current = board->array[i];
         if (current == EMPTY || current == WALL) {
             return 0;
         } else if (current == stone) {
@@ -69,16 +71,16 @@ static int count_reversibles_one_dir(const int x, const int y,
     }
 }
 
-static bool can_put_stone(const int x, const int y,
-                          const BOARD_STATE stone, const BOARD board)
+static bool can_put_stone(const char board_i,
+                          const BOARD_STATE stone, const BOARD * board)
 {
     int d;
-    if (board.array[BOARD_I(x, y)] != EMPTY) {
+    if (board->array[board_i] != EMPTY) {
         return false;
     }
 
     for (d = 0; d < 8; d++) {
-        if (count_reversibles_one_dir(x, y, stone, board, d)) {
+        if (count_reversibles_in_the_direction(board_i, stone, board, d)) {
             return true;
         }
     }
@@ -90,11 +92,44 @@ static void search_next_choices(NODE * node)
     int x, y, d;
     for (x = 1; x < BOARD_HEIGHT - 1; x++) {
         for (y = 1; y < BOARD_WIDTH - 1; y++) {
-            if (can_put_stone(x, y, node->stone, node->board)) {
+            if (can_put_stone(BOARD_I(x, y), node->stone, &node->board)) {
                 push_next_choices(BOARD_I(x, y), node->next_choices);
             }
         }
     }
+}
+
+static void reverse_stone_in_the_direction(const char board_i,
+                                           BOARD_STATE stone,
+                                           BOARD * board,
+                                           const int direction_i)
+{
+    int i = board_i;
+    i += directions[direction_i];
+    while (board->array[i] != stone) {
+        board->array[i] = stone;
+        i += directions[direction_i];
+    }
+}
+
+static void reverse_stone(const char board_i, BOARD_STATE stone,
+                          BOARD * board)
+{
+    int direction_i;
+    for (direction_i = 0; direction_i < 8; direction_i++) {
+        if (count_reversibles_in_the_direction
+            (board_i, stone, board, direction_i)
+            != 0) {
+            reverse_stone_in_the_direction(board_i, stone, board,
+                                           direction_i);
+        }
+    }
+}
+
+static void put_stone(const char board_i, BOARD_STATE stone, BOARD * board)
+{
+    board->array[board_i] = stone;
+    reverse_stone(board_i, stone, board);
 }
 
 static void print_next_choices(const char *next_choices)
@@ -151,6 +186,23 @@ static void print_board(const BOARD board)
     }
 }
 
+static void create_child_node(NODE * node)
+{
+    char number_of_next_choices = count_next_choices(node->next_choices);
+    char i;
+
+    node->children = malloc(sizeof(NODE *) * number_of_next_choices);
+
+    for (i = 0; i < number_of_next_choices; i++) {
+        node->children[i] = malloc(sizeof(NODE));	/* 領域確保 */
+        node->children[i]->stone = ANOTHER_STONE(node->stone);	/* 相手の石として登録 */
+        node->children[i]->board = node->board;	/* 親の盤をコピー */
+        put_stone(node->next_choices[i], node->stone,
+                  &node->children[i]->board);
+        node->children[i]->next_choices[0] = -1;	/* 次の手の配列を初期化 */
+    }
+}
+
 static void init_root_node(NODE * root, const char *argv_1)
 {
     int x, y;
@@ -169,6 +221,7 @@ static void init_root_node(NODE * root, const char *argv_1)
 
     root->stone = argv_1[65] - '0';
     root->next_choices[0] = -1;
+    root->children = NULL;
 }
 
 int main(int argc, char *argv[])
@@ -178,8 +231,16 @@ int main(int argc, char *argv[])
 
     init_root_node(&root, argv[1]);
     search_next_choices(&root);
+
     print_board(root.board);
     print_next_choices(root.next_choices);
+
+    create_child_node(&root);
+
+    print_board(root.children[0]->board);
+    print_board(root.children[1]->board);
+    print_board(root.children[2]->board);
+    print_board(root.children[3]->board);
 
     result_x = BOARD_X(root.next_choices[0]) - 1;
     result_y = BOARD_Y(root.next_choices[0]) - 1;
