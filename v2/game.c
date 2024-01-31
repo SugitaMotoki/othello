e2eb36c59e1836943e5f4de1eb7552dc36197be9
SugitaMotoki <ce235019@meiji.ac.jp>
2024-01-31 21:08:31 +0900

    盤モジュール完成

--- v2/game.c
+++ v2/game.c
@@ -10,5 +10,30 @@ int main(void)
     print_board(&board);
     count_stone(&black, &white, &board);
     printf("b: %d, w: %d\n", black, white);
+
+    put_stone(GET_BOARD_I(2 + 1, 3 + 1), BLACK, &board);
+    print_board(&board);
+    count_stone(&black, &white, &board);
+    printf("b: %d, w: %d\n", black, white);
+
+    put_stone(GET_BOARD_I(2 + 1, 4 + 1), WHITE, &board);
+    print_board(&board);
+    count_stone(&black, &white, &board);
+    printf("b: %d, w: %d\n", black, white);
+
+    put_stone(GET_BOARD_I(2 + 1, 5 + 1), BLACK, &board);
+    print_board(&board);
+    count_stone(&black, &white, &board);
+    printf("b: %d, w: %d\n", black, white);
+
+    if (can_put_stone(GET_BOARD_I(2 + 1, 2 + 1), BLACK, &board) != false) {
+        printf("true\n");
+        print_board(&board);
+        count_stone(&black, &white, &board);
+        printf("b: %d, w: %d\n", black, white);
+    } else {
+        printf("false\n");
+    }
+
     return 0;
 }
--- v2/modules/board.h
+++ v2/modules/board.h
@@ -1,6 +1,8 @@
 #ifndef _INCLUDE_BOARD_
 #define _INCLUDE_BOARD_
 
+#include <stdbool.h>
+
 #define FIELD_SIZE (8)
 
 #define BOARD_WIDTH ((FIELD_SIZE) + 2)
@@ -27,7 +29,18 @@ extern BoardI directions[8];
 
 extern void count_stone(char *black, char *white, const Board * board);
 extern void init_board(Board * board);
-static void print_board_state_icon(const BoardState state);
+extern void print_board_state_icon(const BoardState state);
 extern void print_board(const Board * board);
+extern char count_reverse_in_the_dir(const BoardI board_i,
+                                     const BoardState stone,
+                                     const char dir, const Board * board);
+extern bool can_reverse(const BoardI board_i, const BoardState stone,
+                        const Board * board);
+extern void reverse_stone(const BoardI board_i, const BoardState stone,
+                          Board * board);
+extern bool put_stone(const BoardI board_i, const BoardState stone,
+                      Board * board);
+extern bool can_put_stone(const BoardI board_i, const BoardState stone,
+                          Board * board);
 
 #endif
